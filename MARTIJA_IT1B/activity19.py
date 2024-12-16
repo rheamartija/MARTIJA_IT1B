5ee2d101abb4a63846881d548a78e9deccb7566b
rheamartija <rheamartija09@gmail.com>
2024-12-16 09:16:06 +0800

    Update activity19.py

--- MARTIJA_IT1B/activity19.py
+++ MARTIJA_IT1B/activity19.py
@@ -1,5 +1,17 @@
-modules = 1
+#listing
 
-from activity18 import factorial
+fruits = ["Avocado","Mango","Orange","Rambutan"]
 
-print(f"the factrial of 7 is {factorial(7)}")
+print(f"My favorite fruits is {fruits[3]}.")
+
+#to add another item to the list
+fruits.append("Apple")
+print(fruits)
+
+#to insert an item to a specific index in the list
+fruits.insert(2,"Watermelon")
+print(fruits)
+
+#to delete an item on a specifc index in the lsit
+fruits.remove("Mango")
+print(fruits)
