c0bcd8c7da2964930447ffb37620f29b4bebe73c
rheamartija <rheamartija09@gmail.com>
2024-12-16 08:59:22 +0800

    Update activity2.py

--- MARTIJA_IT1B/activity2.py
+++ MARTIJA_IT1B/activity2.py
@@ -2,8 +2,3 @@
 #comments are ignore by the compiler which is python,nevertheless still an
 #important part of the program
 
-name = input("What is your name?")
-age = input("How old are you?")
-
-
-print( "Hi,", name, age ,"years old")
